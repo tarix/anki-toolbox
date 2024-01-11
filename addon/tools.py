a6cb395a0bc76c2b12f1b07503547bc5a43de72b
Michael Montague <mikem@tarix.net>
2024-01-11 23:39:22 +0900

    add function to convert jalup decks into focus clozes

--- addon/tools.py
+++ addon/tools.py
@@ -7,13 +7,14 @@
 #
 # This project is hosted on GitHub: https://github.com/tarix/anki-toolbox
 
+import csv, importlib, re
+
 from aqt import mw
 from aqt.qt import *
 from aqt.utils import tooltip
+from anki.utils import strip_html
 
 # ----------------------------------------------------------------------------
-
-import csv
 
 def menuAddKLCData():
 
@@ -63,9 +64,84 @@ def menuAddKLCData():
 
 def initAddKLCData():
     # add the menu option
-    mw.form.action = QAction('ATK: Add KLC Data', mw)
+    mw.form.action = QAction('ATB: Add KLC Data', mw)
     mw.form.action.setEnabled(True)
     mw.form.action.triggered.connect(menuAddKLCData)
+    mw.form.menuTools.addAction(mw.form.action)
+
+# -----------------------------------------------------------------------------
+
+_mecab = None
+
+# FIXME: convert to more updated mecab
+def mecab():
+    global _mecab
+    if _mecab == None:
+        # import the japanese plugin mecab module
+        reading = importlib.import_module('3918629684.reading')
+        try:
+            # create our controller and initialize it
+            _mecab = reading.MecabController()
+            _mecab.setup()
+        except:
+            _mecab = None
+    return _mecab
+
+def extract_vocab(notes):
+    # regex to match the vocab
+    pattern1 = r'^(.+?)：'
+    pattern2 = r'^(.+?):'
+    # Use re.MULTILINE to enable matching at the start of each line
+    match = re.search(pattern1, notes, flags=re.MULTILINE)
+    if match == None:
+        match = re.search(pattern2, notes, flags=re.MULTILINE)
+    # see if we found the vocab
+    if match:
+        vocab = match.group(1).strip()
+        return vocab
+    else:
+        return None
+
+def generate_cloze( vocab, ruby ):
+    if vocab == ruby:
+        return f'{{{{c1::{vocab}::{vocab}}}}}'
+    else:
+        return f'{{{{c1:: {ruby}::{vocab}}}}}'
+
+def menuFixJalupDeck():
+    print('Menu: Fix Jalup Deck')
+    notes = mw.col.find_notes('deck:"Jalup::2. Intermediate"')
+    for i, nid in enumerate(notes):
+        # get the next note
+        note = mw.col.get_note(nid)
+        # calculate the stage and card number
+        stage = int(i / 250) + 1
+        cnum = i + 1
+        # check if we are already processed
+        text = strip_html( note['Expression'] )
+        # grab the vocab word
+        vocab = extract_vocab( strip_html(note['Meaning']) )
+        if not vocab == None:
+            # grab the vocab reading
+            ruby = mecab().reading( vocab )
+            # generate the cloze
+            cloze = generate_cloze( vocab, ruby )
+            # replace and update the text
+            text = str.replace( text, vocab, cloze )
+        else:
+            # tag this note as not clozed
+            note.add_tag('ja-jalup-nocloze')
+        # update the note
+        note['Text'] = text
+        note['Source'] = f'Jalup Intermediate Stage {stage} #{cnum}'
+        # save the note changes
+        mw.col.update_note(note)
+
+def initFixJalupDeck():
+    # add the menu option
+    mw.form.action = QAction('ATB: Fix Jalup Deck', mw)
+    mw.form.action.setEnabled(True)
+    mw.form.action.triggered.connect(menuFixJalupDeck)
     mw.form.menuTools.addAction(mw.form.action)
 
 # -----------------------------------------------------------------------------
@@ -73,5 +149,6 @@ def initAddKLCData():
 def init():
     mw.form.menuTools.addSeparator()
     initAddKLCData()
+    initFixJalupDeck()
 
 init()
